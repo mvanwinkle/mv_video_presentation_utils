a8dd457a9f2a2d096f9990ee4a2d2a750bbbf601
Martin VanWinkle III <mvanwinkle@ias.edu>
2022-01-13 10:43:37 -0500

    it works.

--- src/bin/gen_list.sh
+++ src/bin/gen_list.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+input_dir="$1"; shift
+
 function sed_quote_lines_escape_single_quotes
 {
 	sed -e "s/'/'\\\\''/g;s/\(.*\)/'\1'/"
@@ -7,12 +9,13 @@ function sed_quote_lines_escape_single_quotes
 
 function get_file_list_for_ffmpeg
 {
-	dir="$1"; shift
+	local dir="$1"; shift
 
-	find "$dir" -mindepth 1 -type f \
+	find "$dir" -mindepth 1 -type f -print0 \
+		| xargs -0 -n1 realpath \
 		| sort \
 		| sed_quote_lines_escape_single_quotes \
 		| sed 's/^/file /'
 }
 
-get_file_list_for_ffmpeg './input'
+get_file_list_for_ffmpeg "$input_dir"
--- src/bin/process.sh
+++ src/bin/process.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
 # ffmpeg -f concat -safe 0 -i my_list.txt -c copy output.mkv
+# https://trac.ffmpeg.org/wiki/Concatenate
+# The -safe 0 above is not required if the paths are relative.
+# apparently you need to use -safe 0 if you're doing process substitution
 # ffmpeg -i abstract1.mkv abstract1.mp4
 
+input_file="$1" ; shift
+output_directory="$1"; shift
+output_format="$1"; shift;
+output_label="$1" ; shift
+
+if [[ ! -e "$input_file" ]]
+then
+	>&2 echo "First argument is input file."
+	exit 1
+fi
+
+if [[ ! -d "$output_directory" ]]
+then
+	>&2 echo "Second argument is output directory."
+	exit 1
+fi
+
+if [[ -z "$output_format" ]]
+then
+	>&2 echo "Third argument is output format."
+	exit 1
+fi
+
+if [[ -z "$output_label" ]]
+then
+	output_label="unlabeled"
+fi
+
+
+timestamp=$( date +'%Y-%m-%d-%H-%M-%S' )
+
+output_file="${output_directory}/${timestamp}__${output_label}.${output_format}"
+
+ffmpeg -f concat -safe 0 -i "$input_file" -c copy "$output_file"
